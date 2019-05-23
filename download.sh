ddac9907d3c98f57458dffd77886de7f2171eb39
zhangdxchn <zhangdxchn@gmail.com>
2019-05-23 22:04:57 +0800

    add mongodb

--- download.sh
+++ download.sh
@@ -3,4 +3,6 @@
 
 #wget -m -c -np -k -E -p https://www.elastic.co/guide/en/elasticsearch/reference/7.0/index.html
 
-wget -m -c -np -k -E -p https://www.elastic.co/guide/en/elasticsearch/client/java-api/7.1/index.html
+#wget -m -c -np -k -E -p https://www.elastic.co/guide/en/elasticsearch/client/java-api/7.1/index.html
+
+wget -m -c -np -k -E -p https://m.runoob.com/mongodb/
