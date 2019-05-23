626f71422a61cb6ea5f62c19f60bb1371df476ca
zhangdxchn <zhangdxchn@gmail.com>
2019-05-23 11:10:19 +0800

    java api

--- download.sh
+++ download.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 
-wget -m -c -np -k -E -p https://www.elastic.co/guide/en/elasticsearch/reference/7.0/index.html
+#wget -m -c -np -k -E -p https://www.elastic.co/guide/en/elasticsearch/reference/7.0/index.html
+
+wget -m -c -np -k -E -p https://www.elastic.co/guide/en/elasticsearch/client/java-api/7.1/index.html
