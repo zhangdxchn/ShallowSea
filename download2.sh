257a65b80fe028bcd83ab25031337b0ee2223cd3
zhangdxchn <zhangdxchn@gmail.com>
2019-08-14 09:54:07 +0800

    remove temp

--- download2.sh
+++
@@ -1,9 +0,0 @@
-wget \
-     --recursive \
-     --no-clobber \
-     --page-requisites \
-     --html-extension \
-     --convert-links \
-     --restrict-file-names=windows \
-     --domains  \
-     --no-parent \
